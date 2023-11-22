6e6b8017b967959d8533d1386f2220081cbbff26
arha06001 <arha06001@E-PW0234JA>
2023-11-22 10:09:20 +0100

    Add project files.

---
+++ Inlämninguppgift 5.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        Console.WriteLine("skriv in 5 städer");
+
+        string[] städer = new string[5];
+
+        for (int i = 0; i < städer.Length; i++)
+        {
+            Console.WriteLine($"Skriv in stad {i + 1}");
+            städer[i] = Console.ReadLine();
+        }
+
+        Console.WriteLine("Här är de städer du skrev in");
+
+        foreach (string stad in städer)
+        {
+            Console.WriteLine(stad);
+        }
+    }
+}
